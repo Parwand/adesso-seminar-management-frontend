2c9e2a78e5b56d0ee42e04e3e491e9fae7c5ee26
Parwand <parwand.alsino@hhu.de>
2022-11-14 14:06:41 +0100

    implemented method 'getAllSeminarraums' in 'SeminarraumComponent'

--- src/app/components/seminarraum/seminarraum.component.ts
+++ src/app/components/seminarraum/seminarraum.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Seminarraum } from 'src/app/interfaces/Seminarraum';
+import { SeminarraumService } from 'src/app/services/seminarraum/seminarraum.service';
 
 @Component({
   selector: 'app-seminarraum',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seminarraum.component.css']
 })
 export class SeminarraumComponent implements OnInit {
+  public seminarraums: Seminarraum[];
 
-  constructor() { }
+  constructor(private seminarraumService: SeminarraumService) { }
 
   ngOnInit(): void {
+    this.getAllSeminarraums();
+  }
+
+  public getAllSeminarraums() {
+    this.seminarraumService.getAllSeminarraums().subscribe(
+      (response: Seminarraum[]) => {this.seminarraums = response}
+    );
   }
 
 }
