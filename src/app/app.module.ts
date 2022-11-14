acc6f9a4ccd20fc8574df1eed2f5b9ac8c65e0ab
Parwand <parwand.alsino@hhu.de>
2022-11-14 13:27:08 +0100

    generated about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 import { SeminarraumComponent } from './components/seminarraum/seminarraum.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 
-export const appRoutes: Routes = [
-  {path:"about", component: FooterComponent}
-]
 
 @NgModule({
   declarations: [
