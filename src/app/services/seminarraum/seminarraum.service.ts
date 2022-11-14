99b418a1c68ee664778e090ab09bdf2718c764ed
Parwand <parwand.alsino@hhu.de>
2022-11-14 14:02:25 +0100

    implemented method 'getAllSeminarraums' in 'SeminarraumService'

--- src/app/services/seminarraum/seminarraum.service.ts
+++ src/app/services/seminarraum/seminarraum.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Seminarraum } from 'src/app/interfaces/Seminarraum';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeminarraumService {
+  private apiUrl:string = "http://localhost:8080/seminarraum";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { } 
+
+  public getAllSeminarraums(): Observable<Seminarraum[]> {
+    return this.httpClient.get<Seminarraum[]>(`${this.apiUrl}/all`);
+  }
 }
