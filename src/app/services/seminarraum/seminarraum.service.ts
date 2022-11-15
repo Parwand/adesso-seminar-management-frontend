34894f761074fd2a3b2760a4fdda024e47ca66a5
Parwand <parwand.alsino@hhu.de>
2022-11-15 13:20:50 +0100

    uses environment.baseUrl in 'SeminarraumService'

--- src/app/services/seminarraum/seminarraum.service.ts
+++ src/app/services/seminarraum/seminarraum.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Seminarraum } from 'src/app/interfaces/Seminarraum';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeminarraumService {
-  private apiUrl:string = "http://localhost:8080/seminarraum";
+  private apiUrl:string = environment.baseUrl;
 
   constructor(private httpClient: HttpClient) { } 
 
   public getAllSeminarraums(): Observable<Seminarraum[]> {
-    return this.httpClient.get<Seminarraum[]>(`${this.apiUrl}/all`);
+    return this.httpClient.get<Seminarraum[]>(`${this.apiUrl}/seminarraum/all`);
   }
 
   public addSeminarraum(seminarraum: Seminarraum): Observable<Seminarraum> {
-    return this.httpClient.post<Seminarraum>(`${this.apiUrl}/add`, seminarraum);
+    return this.httpClient.post<Seminarraum>(`${this.apiUrl}/seminarraum/add`, seminarraum);
   }
 
   public deleteSeminarraum(raumnummer: number | undefined): Observable<void> {
-    return this.httpClient.delete<void>(`${this.apiUrl}/delete/${raumnummer}`);
+    return this.httpClient.delete<void>(`${this.apiUrl}/seminarraum/delete/${raumnummer}`);
   }
 
   public editSeminarraum(seminarraum: Seminarraum): Observable<Seminarraum> {
-    return this.httpClient.put<Seminarraum>(`${this.apiUrl}/update`, seminarraum);
+    return this.httpClient.put<Seminarraum>(`${this.apiUrl}/seminarraum/update`, seminarraum);
   }
 }
