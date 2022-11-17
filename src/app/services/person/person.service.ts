81562313c91ff4b0906a83ba00a5ae39609cd953
Parwand <parwand.alsino@hhu.de>
2022-11-17 14:40:54 +0100

    implemented method 'seminarbuchungStornieren' in ''SeminarService.ts

--- src/app/services/person/person.service.ts
+++ src/app/services/person/person.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Seminarbuchung } from 'src/app/interfaces/Seminarbuchung';
@@ -14,5 +14,12 @@ export class PersonService {
 
   public getBuchungenByPersonId(personId: number):  Observable<Seminarbuchung[]>{
     return this.httpClient.get<Seminarbuchung[]>(`${this.apiUrl}/person/buchungen/${personId}`);
+  }
+
+  seminarbuchungStornieren(buchungsnummer: string, personId: number): Observable<void> {
+    const params: HttpParams = new HttpParams()
+    .set("buchungsnummer", buchungsnummer)
+    .set("personId", personId);
+    return this.httpClient.post<void>(`${this.apiUrl}/person/buchung/stornieren`, params);
   }
 }
