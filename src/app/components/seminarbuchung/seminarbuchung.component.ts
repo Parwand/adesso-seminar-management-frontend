ad38a9348b546394139ccf13234303cdc6fcdc6f
Parwand <parwand.alsino@hhu.de>
2022-11-17 13:41:58 +0100

    implemented method in 'getBuchungen'

--- src/app/components/seminarbuchung/seminarbuchung.component.ts
+++ src/app/components/seminarbuchung/seminarbuchung.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Seminarbuchung } from 'src/app/interfaces/Seminarbuchung';
+import { PersonService } from 'src/app/services/person/person.service';
 
 @Component({
   selector: 'app-seminarbuchung',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seminarbuchung.component.css']
 })
 export class SeminarbuchungComponent implements OnInit {
-
-  constructor() { }
+  seminarbuchungen: Seminarbuchung[];
+  constructor(private personService: PersonService) { }
 
   ngOnInit(): void {
+    this.getBuchungen();
   }
+
+  public getBuchungen(): void {
+    this.personService.getBuchungenByPersonId(1).subscribe({ 
+      next: (value: Seminarbuchung[]) => {this.seminarbuchungen = value; console.log(value);
+      }, 
+      error: (e: HttpErrorResponse) =>{console.log(e.message);
+      }
+    }
+      
+    );
+  }
+
+
 
 }
