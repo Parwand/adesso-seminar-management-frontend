bb5433d6321e2bc6d13fba5f5de9d91c1dac17cd
Parwand <parwand.alsino@hhu.de>
2022-11-17 08:53:08 +0100

    set undefined in interface 'Seminar.ts'

--- src/app/interfaces/Seminar.ts
+++ src/app/interfaces/Seminar.ts
@@ -1,13 +1,13 @@
 import { Seminarraum } from "./Seminarraum";
 
 export interface Seminar {
-    seminarnummer: number;
-    seminarTitle: string;
-    beginn: Date;
-    ende: Date;
-    kursinhalt: string;
-    teilnehmeranzahl: number;
-    seminarleiter: string;
-    voraussetzung: string;
+    seminarnummer?: number;
+    seminarTitle?: string;
+    beginn?: Date;
+    ende?: Date;
+    kursinhalt?: string;
+    teilnehmeranzahl?: number;
+    seminarleiter?: string;
+    voraussetzung?: string;
     seminarraum?: Seminarraum;
 }
