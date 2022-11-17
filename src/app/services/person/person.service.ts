86791ee4265d7e5c4301899729fdb374e28f1de3
Parwand <parwand.alsino@hhu.de>
2022-11-17 13:36:53 +0100

    implemented method 'getBuchungenByPersonId' in 'PersonServic'

--- src/app/services/person/person.service.ts
+++ src/app/services/person/person.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Seminarbuchung } from 'src/app/interfaces/Seminarbuchung';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
+  apiUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getBuchungenByPersonId(personId: number):  Observable<Seminarbuchung[]>{
+    return this.httpClient.get<Seminarbuchung[]>(`${this.apiUrl}/person/buchungen`);
+  }
 }
