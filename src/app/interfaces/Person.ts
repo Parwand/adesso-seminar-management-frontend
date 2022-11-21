29dafb5a78c63036d0ad4392ec702333367bcca8
Parwand <parwand.alsino@hhu.de>
2022-11-21 09:40:31 +0100

    created interface 'Person.ts'

---
+++ src/app/interfaces/Person.ts
@@ -0,0 +1,14 @@
+import { Adresse } from "./Adresse";
+import { Seminar } from "./Seminar";
+import { Seminarbuchung } from "./Seminarbuchung";
+
+export interface Person{
+    id: number;
+    username: string;
+    firstname: string;
+    lastname: string;
+    geburtsdatum: Date;
+    adresse: Adresse;
+    seminarList: Seminar[];
+    seminarbuchungen: Seminarbuchung[];
+}
