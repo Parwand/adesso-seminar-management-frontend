8db67eee46c4b7fce2161fbb1b08dfc49cb870c4
Parwand <parwand.alsino@hhu.de>
2022-11-22 09:00:52 +0100

    implemented method 'setUser'

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { AuthGuard } from 'src/app/guards/auth.guard';
+import { Person } from 'src/app/interfaces/Person';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +9,21 @@ import { AuthGuard } from 'src/app/guards/auth.guard';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  person: Person;
 
-  constructor(private keycloakService: KeycloakService) { }
+  constructor(private keycloakService: KeycloakService, 
+              private authGuard: AuthGuard) { }
 
   ngOnInit(): void {
+    this.setUser();
   }
 
   public logout(): void {
     this.keycloakService.logout("http://localhost:4200");
   }
+
+  public setUser(): void {
+    this.person = this.authGuard.getLoggedPerson();
+  }
+
 }
